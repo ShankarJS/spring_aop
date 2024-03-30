dc37d91cac224f32a540f84156de64b9e0b29f3b
Shankarlal Sharma <shankarjsharma@gmail.com>
2024-03-31 01:15:53 +0530

    indentation changes

--- lcwd_crashcourse/aopconcept/src/main/java/com/aop/services/PaymentServiceImpl.java
+++ lcwd_crashcourse/aopconcept/src/main/java/com/aop/services/PaymentServiceImpl.java
@@ -2,21 +2,16 @@ package com.aop.services;
 
 public class PaymentServiceImpl implements PaymentService {
 
+	//This is JoinPoint for the Aspect
 	public void makePayment() {
-		// TODO Auto-generated method stub
 		System.out.println("Amount debited..");
-
 		System.out.println("Amount credited..");
 	}
 
 	@Override
 	public void makePayment(int amount) {
-		// TODO Auto-generated method stub
-
 		System.out.println(amount + "Amount debited..");
-
 		System.out.println(amount + "Amount credited..");
-
 	}
 
 }
